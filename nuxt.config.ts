a57fd7ddd0db0a3b288f601cc8085aff707a8036
babiron! <babi@example.com>
2023-06-07 23:04:16 +0900

    とりあえず動く気がする

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    
+    routeRules: {
+        '/api/**': {
+            cors: true,
+            headers: {
+                'access-control-allow-methods': 'GET',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': '*'
+            },
+            proxy : { to : 'https://publish.twitter.com/**' }
+            // proxy : { to : 'https://twitter.com/**' }
+            // proxy : { to : 'https://qiita.com/api/**' }
+        },
+    }
 })
--- pages/[username]/status/[post_id].vue
+++ pages/[username]/status/[post_id].vue
@@ -1,20 +1,6 @@
-<template>
-    <div>
-        <p>{{ $route }}</p>
-        <p>{{ data }}</p>
-    </div>
-</template>
 
 <script setup>
 import { useHead } from "#app";
-
-useHead({
-  title: '<ページタイトル>',
-  meta: [
-    { property: 'og:title', content: '<ページタイトルを記載>' },
-    { property: 'og:description', content: '<descriptionを記載>' }
-  ]
-});
 
 definePageMeta({
     validate: async(route) => {
@@ -25,27 +11,48 @@ definePageMeta({
         if(! (valid_id && valid_username)){
             return false;
         }
-
-        const URL = `https://twitter.com/${route.params.username}/status/${route.params.post_id}`
-        const { data, pending, refresh, error } = await useFetch(URL,{
-            key: "cash", // POINT1
-            //method: METHOD,
-            // credentials: 'include', //POINT3
-            // headers: HEADERS, //POINT4
-            // params: params, // POINT5
-            async onResponse({ request, response, options }) {
-                console.log('onResponse log1', request, response.status)
-                for (const header of response.headers.entries()) {
-                    console.log('onResponse HEADER', header)
-                }
-            }, // POINT2
-        })
-        console.log(data)
-        console.log(pending)
-        console.log(refresh)
-        console.log(error)
+        return true;
     }
 })
 
+const route = useRoute()
+const URL = `/api/oembed?url=https%3A%2F%2Ftwitter.com%2F${route.params.username}%2Fstatus%2F${route.params.post_id}&widget=Tweet`
+const { data } = await useFetch(URL,{key: "cash", onResponse({ request, response, options }) {
+    let response_data = {}
+    // 応答データの処理
+    console.log("============================")
+    console.log(response._data.html.match("<p lang=.+\">(.+)</p>")[0].replace(/<br>/g, "\n").replace(/<.+?>/g, ""))
+    
+    console.log("============================")
+
+    response_data.text = response._data.html.match("<p lang=.+\">(.+)</p>")[0].replace(/<br>/g, "\n").replace(/<.+?>/g, "")
+
+    
+    useHead({
+        title: 'TwitOGP',
+        meta: [
+            { property: 'og:title', content: 'Twitter' },
+            { property: 'og:description', content: response_data.text },
+            { property: 'og:image', content: `https://twitter.com/${route.params.username}/status/${route.params.post_id}/photo/1` },
+            { property: 'og:image', content: `https://twitter.com/${route.params.username}/status/${route.params.post_id}/photo/2` },
+            { property: 'og:image', content: `https://twitter.com/${route.params.username}/status/${route.params.post_id}/photo/3` },
+            { property: 'og:image', content: `https://twitter.com/${route.params.username}/status/${route.params.post_id}/photo/4` },
+        ]
+    });
+
+    return response_data
+}})
+
+
+
+
 
 </script>
+
+
+<template>
+    <div>
+        <p>{{ $route }}</p>
+        <p>{{ data.text }}</p>
+    </div>
+</template>
