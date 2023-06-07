e8bb9d828aa15e0ef4d12187f01c3d7117a142da
babiron! <babi@example.com>
2023-06-07 23:11:03 +0900

    セミコロン入れた

--- pages/[username]/status/[post_id].vue
+++ pages/[username]/status/[post_id].vue
@@ -5,27 +5,27 @@ import { useHead } from "#app";
 definePageMeta({
     validate: async(route) => {
         // post_idが数字以外は認めない
-        let valid_id = /^\d+$/.test(route.params.post_id)
+        let valid_id = /^\d+$/.test(route.params.post_id);
         // usernameが英数字と_以外は認めない
-        let valid_username = /^\w+$/.test(route.params.username)
+        let valid_username = /^\w+$/.test(route.params.username);
         if(! (valid_id && valid_username)){
             return false;
         }
         return true;
     }
-})
+});
 
-const route = useRoute()
-const URL = `/api/oembed?url=https%3A%2F%2Ftwitter.com%2F${route.params.username}%2Fstatus%2F${route.params.post_id}&widget=Tweet`
+const route = useRoute();
+const URL = `/api/oembed?url=https%3A%2F%2Ftwitter.com%2F${route.params.username}%2Fstatus%2F${route.params.post_id}&widget=Tweet`;
 const { data } = await useFetch(URL,{key: "cash", onResponse({ request, response, options }) {
-    let response_data = {}
+    let response_data = {};
     // 応答データの処理
-    console.log("============================")
-    console.log(response._data.html.match("<p lang=.+\">(.+)</p>")[0].replace(/<br>/g, "\n").replace(/<.+?>/g, ""))
+    console.log("============================");
+    console.log(response._data.html.match("<p lang=.+\">(.+)</p>")[0].replace(/<br>/g, "\n").replace(/<.+?>/g, ""));
     
-    console.log("============================")
+    console.log("============================");
 
-    response_data.text = response._data.html.match("<p lang=.+\">(.+)</p>")[0].replace(/<br>/g, "\n").replace(/<.+?>/g, "")
+    response_data.text = response._data.html.match("<p lang=.+\">(.+)</p>")[0].replace(/<br>/g, "\n").replace(/<.+?>/g, "");
 
     
     useHead({
@@ -40,7 +40,7 @@ const { data } = await useFetch(URL,{key: "cash", onResponse({ request, response
         ]
     });
 
-    return response_data
+    return response_data;
 }})
 
 
